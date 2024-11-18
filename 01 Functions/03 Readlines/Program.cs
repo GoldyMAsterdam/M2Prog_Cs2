fc7a5394df7f4a2b861a6823d99e00d444055858
Tycho Boom <38436@ma-web.nl>
2024-11-18 12:58:28 +0100

    Made 00 & 01 & 02 & 03

---
+++ 01 Functions/03 Readlines/Program.cs
@@ -0,0 +1,17 @@
+namespace Readlines
+{
+    class Program
+
+            // Call the Vraag1 method
+            Vraag1();
+        }
+
+        internal static void Vraag1()
+        {
+            Console.WriteLine("How long do you think you'd survive in a zombie apocalypse?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+    }
+}
---
+++ 01 Functions/03 Readlines/Readlines/Readlines/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Readlines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vraag1();
+            Vraag2();
+            Vraag3();
+            Vraag4();
+            Vraag5();
+            Vraag6();
+        }
+
+        internal static void Vraag1()
+        {
+            Console.WriteLine("How long do you think you'd survive in a zombie apocalypse?");
+            string antwoord = Console.ReadLine();
+        }
+        internal static void Vraag2()
+        {
+            Console.WriteLine("What secret conspiracy would you like to actually start letting other people know?");
+            string antwoord = Console.ReadLine();
+        }
+        internal static void Vraag3()
+        {
+            Console.WriteLine("If you can still remember, what are your funniest childhood memories?");
+            string antwoord = Console.ReadLine();
+        }
+        internal static void Vraag4()
+        {
+            Console.WriteLine("What would you do if you won a million dollars?");
+            string antwoord = Console.ReadLine();
+        }
+        internal static void Vraag5()
+        {
+            Console.WriteLine("What is the worst game you played but that you liked anyway?");
+            string antwoord = Console.ReadLine();
+        }
+        internal static void Vraag6()
+        {
+            Console.WriteLine("Which species would be the rudest if all animals could talk?");
+            string antwoord = Console.ReadLine();
+        }
+    }
+}
