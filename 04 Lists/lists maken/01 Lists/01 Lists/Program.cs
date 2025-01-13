0d38396def7c00925ba999c21bdbfc7e5f12dad5
Tycho Boom <38436@ma-web.nl>
2025-01-13 12:11:44 +0100

    Added 04 and finished 00 of 05

--- 04 Lists/lists maken/01 Lists/01 Lists/Program.cs
+++ 04 Lists/lists maken/01 Lists/01 Lists/Program.cs
@@ -12,6 +12,5 @@
             Console.WriteLine("Hello, World!");
         }
         string[] Characters = { "Pac-man", "Samus", "Crash Bandicoot", "Spyro the Dragon", "Donkey Kong", "Mario", "Luigi", "Astro", "Solid Snake", "Master Chief", "Kratos", "Sonic the Hedgehog", "Link" };
-    }
+    }  
 }
-    
---
+++ 05 dictionaries/Dictionaries/Dictionaries/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.IO.Pipes;
+
+namespace Dictionaries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> personeelOpNummer = new Dictionary<int, string>();
+            {
+                personeelOpNummer.Add(382942, "jam");
+                personeelOpNummer.Add(382943, "jim");
+            }
+            string jam = personeelOpNummer[382942];
+            string jim = personeelOpNummer[382943];
+            Console.WriteLine(personeelOpNummer[382942]);
+            Console.WriteLine(personeelOpNummer[382943]);
+
+            foreach (KeyValuePair<int, string> item in personeelOpNummer)
+            {
+                string value = item.Value;
+                Console.WriteLine($"Key: {item.Key}, Value: {value}");
+            }
+        }
+    }
+}
