36e912eb9b979b894105923415c995ef205c56ec
Tycho Boom <38436@ma-web.nl>
2025-01-27 11:21:21 +0100

    Added 05

---
+++ 06/00/Class/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Environment.CurrentDirectory);
+        }
+        string[] lines = File.ReadAllLines("Quiz.txt");
+    }
+}
